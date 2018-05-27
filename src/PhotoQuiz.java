1ad1c17d1fc7818b0e407a88bffeb4c7091b83cb
Asher Tedford <league@WTS-iMac-11.attlocal.net>
2018-05-27 15:59:51 -0700

    DrumKit

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,7 +22,7 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+         
 		// 2. create a variable of type "Component" that will hold your image
 
 		// 3. use the "createImage()" method below to initialize your Component
