76d2ca972fc98a1db9244f790ae1b9343feafde3
Asher Tedford <league@WTS-iMac-11.attlocal.net>
2018-06-03 15:59:46 -0700

    DrumKit, MyFristSwing, DuelingButtons

--- src/DuelingButtons.java
+++ src/DuelingButtons.java
@@ -1,3 +1,4 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 1
@@ -12,60 +13,60 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class DuelingButtons implements ActionListener {
-	
+
 	public static void main(String[] args) {
 		new DuelingButtons().createUI();
 	}
 
 	JButton leftButton = new JButton();
 	JButton rightButton = new JButton();
-	
-	Dimension BIG = new Dimension(400,400);
-	Dimension SMALL = new Dimension(200,200);
-	
+
+	Dimension BIG = new Dimension(400, 400);
+	Dimension SMALL = new Dimension(200, 200);
+
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 
 	private void createUI() {
-		// 1. Add the panel to the frame
 
-		// 2. Make the frame visible
+		frame.add(panel);
 
-		// 3. Set the text of the leftButton to "Click me!"
+		frame.setVisible(true);
 
-		// 4. Set the text of the rightButton to "Click me!"
+		leftButton.setText("Click me!");
 
-		// 5. Add an action listener to the leftButton
+		rightButton.setText("Click me!");
 
-		// 6. Add an action listener to the rightButton
+		leftButton.addActionListener(this);
 
-		// 7. Add the leftButton to the panel
+		rightButton.addActionListener(this);
 
-		// 8. Add the rightButton to the panel
+		panel.add(leftButton);
 
-		// 9. Pack the frame
+		panel.add(rightButton);
 
-		// 10. Set the title of the frame to "Demanding Buttons"
+		frame.pack();
 
+		frame.setTitle("Demanding Buttons");
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		JButton buttonPressed = (JButton) arg0.getSource();
-		
-		
-		/* If the buttonPressed was the leftButton....*/
-			// Set the text of the rightButton to "No, click Me!"
-			// Set the PREFERRED size of the rightButton to BIG
-			// Set the text of the leftButton to "Click Me!"
-			// Set the PREFERRED size of the leftButton to SMALL
-		
-		
-		/* If the buttonPressed was the rightButton, do the opposite. */
-		
 
+		if (buttonPressed.equals(leftButton)) {
+			rightButton.setText("No, click Me!");
+			rightButton.setPreferredSize(BIG);
+			leftButton.setText("Click Me!");
+			leftButton.setPreferredSize(SMALL);
+		}
+
+		if (buttonPressed.equals(rightButton)) {
+			leftButton.setText("No, click Me!");
+			leftButton.setPreferredSize(BIG);
+			rightButton.setText("Click Me!");
+			rightButton.setPreferredSize(SMALL);
+		}
 		frame.pack();
 	}
 }
-
-
